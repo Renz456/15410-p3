28fe499c490339d3ad25b6742e1a5613e2e4d228
Abhinav Gupta <abhinav6@andrew.cmu.edu>
2024-03-12 17:57:57 -0400

    filled in wrapper func

--- kern/thread_obj.c
+++ kern/thread_obj.c
@@ -13,13 +13,24 @@
 
 void insert_thread(thread_obj_t *head, thread_obj_t *tail, thread_obj_t *thread)
 {
+    thread->next = NULL;
+    thread->prev = tail;
+    tail->next = thread;
+    tail = thread;
 }
 
 void remove_thread(thread_obj_t *head, thread_obj_t *tail, thread_obj_t *thread)
 {
+    head = head->next;
+    head->prev = NULL;
 }
 
 thread_obj_t *find_thread(thread_obj_t *head, thread_obj_t *tail, int tid)
 {
+    for(thread_obj_t *start = head; start != NULL; start = start->next){
+        if(start->tid == tid){
+            return start;
+        }
+    }
     return NULL;
 }
