365f0bec71fc74c8b3d8079793f84b0530bbaadb
Abhinav Gupta <abhinav6@andrew.cmu.edu>
2024-03-12 18:30:44 -0400

    changed linked list a little bit

--- kern/thread_obj.c
+++ kern/thread_obj.c
@@ -13,16 +13,28 @@
 
 void insert_thread(thread_obj_t *head, thread_obj_t *tail, thread_obj_t *thread)
 {
+    assert(thread != NULL);
     thread->next = NULL;
     thread->prev = tail;
+    if(tail == NULL){
+        head = thread;
+        tail = thread;
+        return;
+    }
     tail->next = thread;
     tail = thread;
 }
 
 void remove_thread(thread_obj_t *head, thread_obj_t *tail, thread_obj_t *thread)
 {
+    assert(thread != NULL && head != NULL);
     head = head->next;
-    head->prev = NULL;
+    if(head != NULL){
+        head->prev = NULL;
+    }else{
+        tail = NULL;
+    }
+    return;
 }
 
 thread_obj_t *find_thread(thread_obj_t *head, thread_obj_t *tail, int tid)
